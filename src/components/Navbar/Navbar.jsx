26f94ccb3ddb831980cbba555ca852822806cc89
malika7177 <malikadjrv@gmail.com>
2024-08-24 20:19:15 +0500

    e-commerce website

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        {/* upper navbar */}
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar
