8be60502c184c6853b80c0e0c45385f238cd1c9b
malika7177 <malikadjrv@gmail.com>
2024-08-25 21:01:06 +0500

    upper navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 const Navbar = () => {
   return (
     <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
@@ -35,7 +36,10 @@ const Navbar = () => {
               </span>
               <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
             </button>
-            
+            {/* darkmode switch */}
+            <div>
+                <DarkMode />
+            </div>
           </div>
         </div>
       </div>
