b0a55b3c088190daf1464aeec4f2abe38c4596bf
malika7177 <malikadjrv@gmail.com>
2024-08-26 15:43:10 +0500

    products section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
+import Aos from "aos";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    })
+    Aos.refresh();
+  }, [])
   return <div>
     <Navbar />
     <Hero />
